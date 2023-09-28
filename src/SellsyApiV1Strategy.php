85a52814b3e3cd700789a2a276db3eb61f5aaa41
Virgile <vg.dombox@gmail.com>
2023-09-28 15:37:49 +0200

    fix strategy to cache do_in form data request

--- src/SellsyApiV1Strategy.php
+++ src/SellsyApiV1Strategy.php
@@ -14,23 +14,46 @@ class SellsyApiV1Strategy extends GreedyCacheStrategy
 
     protected function getCacheKey(RequestInterface $request, KeyValueHttpHeader $varyHeaders = null)
     {
-        if (null === $varyHeaders || $varyHeaders->isEmpty()) {
-            return hash(
-                'sha256',
-                'greedy'.$request->getMethod().$request->getUri().json_encode($request->getBody())
-            );
-        }
+		$do_in_content = $this->extractSellsyDoInContent($request);
+		if (null === $varyHeaders || $varyHeaders->isEmpty()) {
+			return hash(
+				'sha256',
+				'greedy'.$request->getMethod().$request->getUri().json_encode($do_in_content)
+			);
+		}
 
-        $cacheHeaders = [];
-        foreach ($varyHeaders as $key => $value) {
-            if ($request->hasHeader($key)) {
-                $cacheHeaders[$key] = $request->getHeader($key);
-            }
-        }
+		$cacheHeaders = [];
+		foreach ($varyHeaders as $key => $value) {
+			if ($request->hasHeader($key)) {
+				$cacheHeaders[$key] = $request->getHeader($key);
+			}
+		}
 
-        return hash(
-            'sha256',
-            'greedy'.$request->getMethod().$request->getUri().json_encode($cacheHeaders).json_encode($request->getBody())
-        );
-    }
+		return hash(
+			'sha256',
+			'greedy'.$request->getMethod().$request->getUri().json_encode($cacheHeaders).json_encode($do_in_content)
+		);
+	}
+
+	protected function extractSellsyDoInContent(RequestInterface $request)
+	{
+		$body = $request->getBody();
+		$body->rewind();
+
+		$boundary = $body->getBoundary();
+
+		$form_data_blocks = preg_split("/-+$boundary/", $body->getContents());
+		array_pop($form_data_blocks);
+
+		foreach ($form_data_blocks as $form_data_block)
+		{
+			if (empty($form_data_block)) {continue;}
+
+			preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $form_data_block, $matches);
+
+			if($matches[1] == 'do_in') {return $matches[2];}
+		}
+
+		return uniqid();//Don't cache
+	}
 }
